ac9da2155d3c48733804695e7b01c2f2c9a307f2
Alexander Niebuhr <alexander@nbhr.io>
2024-02-20 19:30:48 +0100

    fix: build packages

---
+++ builds/astro.ts
@@ -0,0 +1,16 @@
+import { runInRepo } from '../utils.ts';
+import type { RunOptions } from '../types.d.ts';
+
+export async function build(options: RunOptions) {
+	return runInRepo({
+		...options,
+		repo: 'withastro/astro',
+		build: 'build',
+	});
+}
+
+export const packages = {
+	'@astrojs/internal-helpers': 'packages/internal-helpers',
+	'@astrojs/markdown-remark': 'packages/markdown/remark',
+	'@astrojs/telemetry': 'packages/telemetry',
+};
--- tests/adapters.ts
+++ tests/adapters.ts
@@ -5,6 +5,11 @@ export async function test(options: RunOptions) {
 	await runInRepo({
 		...options,
 		repo: 'withastro/adapters',
+		overrides: {
+			'@astrojs/internal-helpers': true,
+			'@astrojs/markdown-remark': true,
+			'@astrojs/telemetry': true,
+		},
 		branch: 'main',
 		build: 'build:ci',
 		test: 'test',
